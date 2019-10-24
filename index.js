bf611abfda815288275e296ff9f97de82846af72
sidaaryan7 <sidaaryan7@gmail.com>
2019-10-24 10:26:48 +0530

    writing changes made on 22nd oct 2019

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +10,13 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+
+app.post("/", function (req, res) {
+    // console.log(req.body.crypto);
+    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function (error, response, body) {
+        console.log(body);
+    })
+});
 
 app.listen(3000, function () {
 
